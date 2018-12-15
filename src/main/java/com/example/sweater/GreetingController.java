1a0faf9fc8409432c0e08cffa1c04c3ce80075fa
Anton Bogolepov <Anton.Bogolepov@leroymerlin.ru>
2018-12-15 20:03:09 +0300

    Beginning of the project: Greeting, controller and template

---
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,29 @@
+package com.example.sweater;
+
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @Autowired
+    private MessageRepo;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        messageRepo.findAll();
+        model.put("messages", "hello, man!");
+        return "main";
+    }
+}
