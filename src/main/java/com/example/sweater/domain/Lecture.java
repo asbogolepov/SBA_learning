f6d1554d1fe32729ee67940ecd636018ecb1dbd7
asbogolepov <antonsergeevich25@gmail.com>
2019-11-08 12:41:18 +0300

    appropriate datatype for lectures

--- src/main/java/com/example/sweater/domain/Lecture.java
+++ src/main/java/com/example/sweater/domain/Lecture.java
@@ -13,11 +13,15 @@ public class Lecture {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String lectureName;
+    @Column(columnDefinition = "text")
     private String lectureContent;
+    private String lectureTechName;
 
     public Lecture(String lectureName, String lectureContent) {
+        this.id = id;
         this.lectureName = lectureName;
         this.lectureContent = lectureContent;
+        this.lectureTechName = lectureName.toLowerCase().replace(' ', '-');
     }
 
 
@@ -42,4 +46,9 @@ public class Lecture {
     public void setLectureContent(String lectureContent) {
         this.lectureContent = lectureContent;
     }
+
+    public String getLectureTechName() {
+        return lectureTechName;
+    }
+
 }
